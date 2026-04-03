ea8d5eed45f05bb136604117dc125fb498da6ea1
B-Patt <brett.patterson@bsu.edu>
2026-04-03 03:40:55 +0000

    setup project

---
+++ PackingList/ConsoleUI.cs
@@ -0,0 +1,25 @@
+﻿namespace PackingList;
+
+using Spectre.Console;
+
+public class ConsoleUI
+{
+    public void Show()
+    {
+        string choice;
+
+        do
+        {
+           choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Packing List")
+                .AddChoices(
+                    "Create New Packing List",
+                    "Load Existing Packing List",
+                    "Manage Saved Packing Lists",
+                    "Exit"));
+
+        } while (choice != "Exit");
+    }
+}
+
