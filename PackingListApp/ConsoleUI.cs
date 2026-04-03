b6dce50887501a8ae8c5105a3a7e42c55da2e6ea
B-Patt <brett.patterson@bsu.edu>
2026-04-03 06:11:46 +0000

    create new packing list

--- PackingListApp/ConsoleUI.cs
+++ PackingListApp/ConsoleUI.cs
@@ -4,6 +4,7 @@ using Spectre.Console;
 
 public class ConsoleUI
 {
+    private PackingListManager manager = new PackingListManager();
     public void Show()
     {
         string choice;
@@ -18,8 +19,29 @@ public class ConsoleUI
                     "Load Existing Packing List",
                     "Manage Saved Packing Lists",
                     "Exit"));
+            
+            if(choice == "Create New Packing List")
+            {
+                CreateNewPackingList()
+            }
 
         } while (choice != "Exit");
     }
+
+    private void CreateNewPackingList()
+    {
+        string name = AnsiConsole.Ask<string>("Enter new list name: ");
+
+        PackingList newList = manager.CreateNewList(name);
+
+        if (newList == null)
+        {
+            AnsiConsole.WriteLine("Invalid list name");
+            return;
+        }
+        AnsiConsole.WriteLine("List Created.");
+    }
+
+
 }
 
