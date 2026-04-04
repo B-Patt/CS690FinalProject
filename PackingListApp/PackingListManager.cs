e53e65cc7f6f51fbfb99e902cf2dba4ab08ba815
B-Patt <brett.patterson@bsu.edu>
2026-04-04 21:59:49 +0000

    packing list manager update

--- PackingListApp/PackingListManager.cs
+++ PackingListApp/PackingListManager.cs
@@ -2,12 +2,54 @@ namespace PackingListApp;
 
 public class PackingListManager
 {
+    private PackingListRepository repository;
+
+    public PackingListManager()
+    {
+        TextFileStorage storage = new TextFileStorage("PackingLists");
+
+        repository = new PackingListRepository(storage);
+    }
+
+
     public PackingList CreateNewList(string name)
     {
         if (!Validator.IsValidListName(name))
+        {
             return null;
+        }
 
-        return new PackingList(name);
+        //Check Duplicates
+        List<string> existing = repository.ListAll();
+        for (int i = 0; i < existing.Count; i++)
+        {
+            if (existing[i].ToLower() == name.ToLower())
+            {
+                return null;
+            }
+        }
+
+        PackingList list = new PackingList(name);
+
+        repository.SaveList(list);
+
+        return list;
     }
+
+    public PackingList LoadList(string name)
+    {
+        return repository.LoadList(name);
+    }
+
+    public void DeleteList(string name)
+    {
+        repository.DeleteList(name);
+    }
+
+    public List<string> ListAllLists()
+    {
+        return repository.ListAll();
+    }
+
 }
 
