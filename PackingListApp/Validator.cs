36a404313ea16a9ec2deab032ed5afe8a9dc876d
B-Patt <brett.patterson@bsu.edu>
2026-04-04 22:25:14 +0000

    Validator initial code

--- PackingListApp/Validator.cs
+++ PackingListApp/Validator.cs
@@ -4,6 +4,38 @@ public static class Validator
 {
     public static bool IsValidListName(string name)
     {
-        return !string.IsNullOrWhiteSpace(name);
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return false;
+        }
+        
+        char[] invalidChars = { '\\', '/', ':', '*', '?', '"', '<', '>', '|' };
+
+        for (int i = 0; i < invalidChars.Length; i++)
+        {
+            if (name.Contains(invalidChars[i]))
+            {
+                return false;
+            }
+        }
+        
+        bool hasLetterOrDigit = false;
+
+        for (int i = 0; i < name.Length; i++)
+        {
+            if (char.IsLetterOrDigit(name[i]))
+            {
+                hasLetterOrDigit = true;
+                break;
+            }
+        }
+
+        if (!hasLetterOrDigit)
+        {
+            return false;
+        }
+
+        return true;
     }
+
 }
